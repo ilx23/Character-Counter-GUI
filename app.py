f5d2b614717842ee8da868f5044c8e9765fc2ae5
Ilia Keshavarz <91822811+ilx23@users.noreply.github.com>
2024-04-09 16:56:28 +0330

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from tkinter import messagebox
 
 
 def char_count():
+    """Count the number of characters in the input text and display the count."""
     text = text_entry.get("1.0", END)
+    # Subtracting 1 to exclude the newline character added by Text widget
     count = len(text) - 1
     char_count_label.config(text=f"Character Count: {count}")
+
+
+def clear_text():
+    """Clear the input text and reset the character count."""
+    text_entry.delete("1.0", END)
+    char_count_label.config(text="Character Count: 0")
 
 
 app = Tk()
@@ -26,6 +34,9 @@ char_count_label = Label(app, font=('aria', 12), fg="#212121")
 char_count_label.pack(pady=15)
 
 count_button = Button(app, text="Count", font=('aria', 13), width=11, borderwidth=2, relief=GROOVE, command=char_count)
-count_button.pack()
+count_button.pack(side=LEFT, padx=80)
+
+clear_button = Button(app, text="Clear", font=('aria', 13), width=11, borderwidth=2, relief=GROOVE, command=clear_text)
+clear_button.pack(side=LEFT)
 
 app.mainloop()
